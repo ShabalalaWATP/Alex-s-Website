4a100cdc604053788aaa5fc81f4d76c3b7ad576a
ShabalalaWATP <128953080+ShabalalaWATP@users.noreply.github.com>
2024-05-20 02:39:44 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,145 +1,131 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('space-invaders');
+    const ctx = canvas.getContext('2d');
 
-const spaceship = {
-    x: canvas.width / 2 - 25,
-    y: canvas.height - 70,
-    width: 50,
-    height: 50,
-    dx: 5
-};
+    const alienRowCount = 3;
+    const alienColumnCount = 10;
+    const alienWidth = 50;
+    const alienHeight = 30;
+    const alienPadding = 10;
+    const alienOffsetTop = 30;
+    const alienOffsetLeft = 30;
+    const playerHeight = 30;
+    const playerWidth = 50;
+    const playerSpeed = 7;
+    const bulletWidth = 4;
+    const bulletHeight = 10;
+    const bulletSpeed = 7;
 
-const bullet = {
-    x: spaceship.x + spaceship.width / 2 - 2.5,
-    y: spaceship.y - 10,
-    width: 5,
-    height: 10,
-    dy: 5,
-    active: false
-};
+    let rightPressed = false;
+    let leftPressed = false;
+    let spacePressed = false;
+    let aliens = [];
+    let bullets = [];
+    let playerX = (canvas.width - playerWidth) / 2;
 
-const invaderRowCount = 3;
-const invaderColumnCount = 8;
-const invaderWidth = 50;
-const invaderHeight = 50;
-const invaderPadding = 10;
-const invaderOffsetTop = 30;
-const invaderOffsetLeft = 30;
-let invaders = [];
-
-for (let c = 0; c < invaderColumnCount; c++) {
-    invaders[c] = [];
-    for (let r = 0; r < invaderRowCount; r++) {
-        invaders[c][r] = { x: 0, y: 0, status: 1 };
-    }
-}
-
-let rightPressed = false;
-let leftPressed = false;
-let spacePressed = false;
-
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-        rightPressed = true;
-    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-        leftPressed = true;
-    } else if (e.key === ' ' || e.key === 'Spacebar') {
-        if (!bullet.active) {
-            bullet.x = spaceship.x + spaceship.width / 2 - bullet.width / 2;
-            bullet.y = spaceship.y - bullet.height;
-            bullet.active = true;
+    for (let c = 0; c < alienColumnCount; c++) {
+        aliens[c] = [];
+        for (let r = 0; r < alienRowCount; r++) {
+            aliens[c][r] = { x: 0, y: 0, status: 1 };
         }
     }
-}
 
-function keyUpHandler(e) {
-    if (e.key === 'Right' || e.key === 'ArrowRight') {
-        rightPressed = false;
-    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-        leftPressed = false;
-    }
-}
-
-function drawSpaceship() {
-    ctx.beginPath();
-    ctx.rect(spaceship.x, spaceship.y, spaceship.width, spaceship.height);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawBullet() {
-    if (bullet.active) {
-        ctx.beginPath();
-        ctx.rect(bullet.x, bullet.y, bullet.width, bullet.height);
-        ctx.fillStyle = 'red';
-        ctx.fill();
-        ctx.closePath();
-    }
-}
-
-function drawInvaders() {
-    for (let c = 0; c < invaderColumnCount; c++) {
-        for (let r = 0; r < invaderRowCount; r++) {
-            if (invaders[c][r].status === 1) {
-                const invaderX = c * (invaderWidth + invaderPadding) + invaderOffsetLeft;
-                const invaderY = r * (invaderHeight + invaderPadding) + invaderOffsetTop;
-                invaders[c][r].x = invaderX;
-                invaders[c][r].y = invaderY;
-                ctx.beginPath();
-                ctx.rect(invaderX, invaderY, invaderWidth, invaderHeight);
-                ctx.fillStyle = 'green';
-                ctx.fill();
-                ctx.closePath();
-            }
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Right' || e.key === 'ArrowRight') {
+            rightPressed = true;
+        } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+            leftPressed = true;
+        } else if (e.key === ' ' || e.key === 'Spacebar') {
+            spacePressed = true;
         }
-    }
-}
+    });
 
-function moveSpaceship() {
-    if (rightPressed && spaceship.x < canvas.width - spaceship.width) {
-        spaceship.x += spaceship.dx;
-    } else if (leftPressed && spaceship.x > 0) {
-        spaceship.x -= spaceship.dx;
-    }
-}
-
-function moveBullet() {
-    if (bullet.active) {
-        bullet.y -= bullet.dy;
-        if (bullet.y < 0) {
-            bullet.active = false;
+    document.addEventListener('keyup', (e) => {
+        if (e.key === 'Right' || e.key === 'ArrowRight') {
+            rightPressed = false;
+        } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+            leftPressed = false;
+        } else if (e.key === ' ' || e.key === 'Spacebar') {
+            spacePressed = false;
         }
-    }
-}
+    });
 
-function collisionDetection() {
-    for (let c = 0; c < invaderColumnCount; c++) {
-        for (let r = 0; r < invaderRowCount; r++) {
-            const invader = invaders[c][r];
-            if (invader.status === 1) {
-                if (bullet.x > invader.x && bullet.x < invader.x + invaderWidth &&
-                    bullet.y > invader.y && bullet.y < invader.y + invaderHeight) {
-                    bullet.active = false;
-                    invader.status = 0;
+    function drawAliens() {
+        for (let c = 0; c < alienColumnCount; c++) {
+            for (let r = 0; r < alienRowCount; r++) {
+                if (aliens[c][r].status === 1) {
+                    const alienX = c * (alienWidth + alienPadding) + alienOffsetLeft;
+                    const alienY = r * (alienHeight + alienPadding) + alienOffsetTop;
+                    aliens[c][r].x = alienX;
+                    aliens[c][r].y = alienY;
+                    ctx.beginPath();
+                    ctx.rect(alienX, alienY, alienWidth, alienHeight);
+                    ctx.fillStyle = '#00ff00';
+                    ctx.fill();
+                    ctx.closePath();
                 }
             }
         }
     }
-}
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSpaceship();
-    drawBullet();
-    drawInvaders();
-    moveSpaceship();
-    moveBullet();
-    collisionDetection();
-    requestAnimationFrame(draw);
-}
+    function drawPlayer() {
+        ctx.beginPath();
+        ctx.rect(playerX, canvas.height - playerHeight, playerWidth, playerHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+    }
 
-draw();
+    function movePlayer() {
+        if (rightPressed && playerX < canvas.width - playerWidth) {
+            playerX += playerSpeed;
+        } else if (leftPressed && playerX > 0) {
+            playerX -= playerSpeed;
+        }
+    }
+
+    function drawBullets() {
+        bullets.forEach((bullet, index) => {
+            ctx.beginPath();
+            ctx.rect(bullet.x, bullet.y, bulletWidth, bulletHeight);
+            ctx.fillStyle = '#ff0000';
+            ctx.fill();
+            ctx.closePath();
+            bullet.y -= bulletSpeed;
+
+            if (bullet.y + bulletHeight < 0) {
+                bullets.splice(index, 1);
+            }
+
+            aliens.forEach((column) => {
+                column.forEach((alien) => {
+                    if (alien.status === 1 && bullet.x > alien.x && bullet.x < alien.x + alienWidth && bullet.y > alien.y && bullet.y < alien.y + alienHeight) {
+                        alien.status = 0;
+                        bullet.status = 0;
+                    }
+                });
+            });
+
+            bullets = bullets.filter(bullet => bullet.status !== 0);
+        });
+    }
+
+    function shootBullet() {
+        if (spacePressed) {
+            bullets.push({ x: playerX + playerWidth / 2 - bulletWidth / 2, y: canvas.height - playerHeight - bulletHeight, status: 1 });
+            spacePressed = false;
+        }
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawAliens();
+        drawPlayer();
+        drawBullets();
+        movePlayer();
+        shootBullet();
+        requestAnimationFrame(draw);
+    }
+
+    draw();
+});
